f03b1f6b4d617a18dae71c3132d2dab998b7d862
Asher Romanenghi <gromanenghi04@gmail.com>
2025-11-20 12:38:05 -0700

    adds halmabot2000

--- halma.py
+++ halma.py
@@ -1,15 +1,26 @@
+from mimetypes import init
 import sys
 from copy import deepcopy
 import tkinter as tk
+import math
 
 # rules
 FIRST_ROW_PAWN_COUNT = 4
 
 
 class Halma:
-    def __init__(self, grid_size: int, timeout: int, player_color: str):
+    def __init__(
+        self,
+        grid_size: int,
+        timeout: int,
+        player_color: str,
+        starting_grid: list[list[int]] | None = None,
+        headless: bool = False,
+    ):
         # game logical grid
-        self.grid: list[list[int]] = self._initialize_grid(grid_size)
+        self.grid: list[list[int]] = (
+            starting_grid if starting_grid else self._initialize_grid(grid_size)
+        )
         # time allowed to make move before timeout
         self.timeout: int = timeout
         # time before current turn is over
@@ -20,6 +31,8 @@ class Halma:
         self.grid_display: list[list[int]] = deepcopy(self.grid)
         # pawns (ids) displayed in tkinter window
         self.pawns: list[list[tk.Canvas]] = []
+        # to draw or not to draw, that is the question
+        self.headless: bool = headless
 
         self.player_1_score: float = 0
         self.player_2_score: float = 0
@@ -29,64 +42,69 @@ class Halma:
         self.previous_square: tuple[int, int] = tuple()  # previous pawn position
         self.after_timer_decrement: str = ""
 
-        # tkinter graphical setup
-        self.display: tk.Tk = tk.Tk()
-        self.display.title("Halma")
-        self.grid_options: dict[int, tuple[str, str]] = {
-            0: ("", ""),  # empty square
-            1: ("black", player_color),  # player 1 / human color
-            2: ("black", "black"),  # player 2 / ai color
-            3: ("black", "cyan"),  # highlighted possible moves colors
-            4: ("black", "gold"),  # selected pawn color
-            5: ("black", "gray"),  # previous move
-        }
-        # set up row labels
-        for row_index in range(grid_size):
-            row_label = tk.Label(self.display, text=str(row_index + 1))
-            row_label.grid(row=row_index + 1, column=0)
+        if not headless:
+            # tkinter graphical setup
+            self.display: tk.Tk = tk.Tk()
+            self.display.title("Halma")
+            self.grid_options: dict[int, tuple[str, str]] = {
+                0: ("", ""),  # empty square
+                1: ("black", player_color),  # player 1 / human color
+                2: ("black", "black"),  # player 2 / ai color
+                3: ("black", "cyan"),  # highlighted possible moves colors
+                4: ("black", "gold"),  # selected pawn color
+                5: ("black", "gray"),  # previous move
+            }
+            # set up row labels
+            for row_index in range(grid_size):
+                row_label = tk.Label(self.display, text=str(row_index + 1))
+                row_label.grid(row=row_index + 1, column=0)
 
-        # set up column labels
-        for col_index in range(grid_size):
-            label_character = chr(ord("a") + col_index)
-            col_label = tk.Label(self.display, text=label_character)
-            col_label.grid(row=grid_size + 1, column=col_index + 1)
+            # set up column labels
+            for col_index in range(grid_size):
+                label_character = chr(ord("a") + col_index)
+                col_label = tk.Label(self.display, text=label_character)
+                col_label.grid(row=grid_size + 1, column=col_index + 1)
 
-        # set up status bar
-        self.player_turn_display: tk.Label = tk.Label(
-            self.display, text="Player 1's turn"
-        )
-        self.player_turn_display.grid(row=0, column=1, columnspan=grid_size // 4)
+            # set up status bar
+            self.player_turn_display: tk.Label = tk.Label(
+                self.display, text="Player 1's turn"
+            )
+            self.player_turn_display.grid(row=0, column=1, columnspan=grid_size // 4)
 
-        self.timer_display: tk.Label = tk.Label(
-            self.display, text=f"Time: {self.time_remaining}"
-        )
-        self.timer_display.grid(
-            row=0, column=(grid_size // 4) + 1, columnspan=grid_size // 4
-        )
+            self.timer_display: tk.Label = tk.Label(
+                self.display, text=f"Time: {self.time_remaining}"
+            )
+            self.timer_display.grid(
+                row=0, column=(grid_size // 4) + 1, columnspan=grid_size // 4
+            )
 
-        self.player_1_score_display: tk.Label = tk.Label(self.display)
-        self.player_1_score_display.grid(
-            row=0, column=(grid_size // 4) * 2 + 1, columnspan=grid_size // 4
-        )
+            self.player_1_score_display: tk.Label = tk.Label(self.display)
+            self.player_1_score_display.grid(
+                row=0, column=(grid_size // 4) * 2 + 1, columnspan=grid_size // 4
+            )
 
-        self.player_2_score_display: tk.Label = tk.Label(self.display)
+            self.player_2_score_display: tk.Label = tk.Label(self.display)
 
-        self._set_player_scores()
+            self._set_player_scores()
 
-        self.player_2_score_display.grid(
-            row=0, column=(grid_size // 4) * 3 + 1, columnspan=grid_size // 4
-        )
+            self.player_2_score_display.grid(
+                row=0, column=(grid_size // 4) * 3 + 1, columnspan=grid_size // 4
+            )
 
-        # setup input box
-        self.move_input: tk.Entry = tk.Entry(self.display)
-        self.move_input.bind("<Return>", self._process_move_input)
-        self.move_input.grid(row=grid_size + 2, column=0, columnspan=grid_size // 2)
+            # setup input box
+            self.move_input: tk.Entry = tk.Entry(self.display)
+            _ = self.move_input.bind(
+                "<Return>", lambda _: self._process_move_input(robo_move=None)
+            )
+            self.move_input.grid(row=grid_size + 2, column=0, columnspan=grid_size // 2)
 
-        # setup error messages
-        self.error_message: tk.Label = tk.Label(self.display, fg="red")
-        self.error_message.grid(
-            row=grid_size + 2, column=grid_size // 2, columnspan=grid_size // 2 + 1
-        )
+            # setup error messages
+            self.error_message: tk.Label = tk.Label(self.display, fg="red")
+            self.error_message.grid(
+                row=grid_size + 2, column=grid_size // 2, columnspan=grid_size // 2 + 1
+            )
+        else:
+            self.display = None
 
     def start_game(self):
         self._initialize_tkinter_grid()
@@ -116,9 +134,12 @@ class Halma:
             if self._check_victory(player):
                 self._end_game(winning_player=player)
 
-    def _process_move_input(self, _):
-        player_input = self.move_input.get()
-        self.move_input.delete(0, tk.END)
+    def _process_move_input(self, robo_move: str | None) -> None:
+        if robo_move:
+            player_input = robo_move
+        else:
+            player_input = self.move_input.get()
+            self.move_input.delete(0, tk.END)
         column_base_value = ord("a")
         try:  # some very suspicious and list unpacking
             starting_coordinate, dest_coordinate = player_input.strip().split("->")
@@ -147,7 +168,9 @@ class Halma:
 
         if self.grid[selected_row][selected_col] != self.player_turn:
             self.selected = (-1, -1)
-            _ = self.error_message.config(text="Invalid move")
+
+            if self.display:
+                _ = self.error_message.config(text="Invalid move")
             return None  # piece is not current players'
 
         self.selected = (selected_row, selected_col)
@@ -192,7 +215,7 @@ class Halma:
                     move_col = col_index + delta_col
                     self.grid_display[move_row][move_col] = 3  # 3 is highlight
                     if (move_row, move_col) not in explored_squares and (
-                        delta_col == 2 or delta_row == 2
+                        abs(delta_col) == 2 or abs(delta_row) == 2
                     ):
                         self._highlight_valid_moves_for_square(
                             move_row, move_col, explored_squares, True
@@ -200,11 +223,6 @@ class Halma:
 
     def _check_victory(self, player: int):
         opponent = 2 if player == 1 else 1
-
-        print("GAME")
-        print("\n".join((str(row) for row in self.grid)), sep="\n")
-        print("CAMPS")
-        print("\n".join((str(row) for row in self.camps)), sep="\n")
 
         piece_coordinates = [
             (row_index, col_index)
@@ -245,10 +263,14 @@ class Halma:
                     "<Button-1>",
                     lambda _, row=row_index, col=col_index: self.make_move(row, col),
                 )
-            _ = self.display.grid_rowconfigure(row_index + 1, weight=0)
+            if self.display:
+                _ = self.display.grid_rowconfigure(row_index + 1, weight=0)
             self.pawns.append(row_pawns)
 
     def _redraw_tkinter_grid(self):
+        if self.headless:
+            return
+
         for row_index, row in enumerate(self.pawns):
             for col_index, pawn in enumerate(row):
                 square_state = self.grid_display[row_index][col_index]
@@ -293,11 +315,16 @@ class Halma:
         return tuple((tuple(row) for row in grid))
 
     def _swap_turns(self):
-        self.display.after_cancel(self.after_timer_decrement)
+        if self.display:
+            self.display.after_cancel(self.after_timer_decrement)
         self.time_remaining = self.timeout
         self.selected = (-1, -1)
         self.player_turn = 1 if self.player_turn == 2 else 2
-        _ = self.player_turn_display.config(text=f"Player {self.player_turn}'s turn")
+
+        if self.display:
+            _ = self.player_turn_display.config(
+                text=f"Player {self.player_turn}'s turn"
+            )
 
         # remove highlighted "possible move" squares
         self.grid_display = [
@@ -306,11 +333,19 @@ class Halma:
         ]
 
         self.turn_number += 1
-        _ = self.error_message.config(text="")
         self._set_player_scores()
-        self._redraw_tkinter_grid()
-        self.after_timer_decrement = self.display.after(1000, self._decrement_timer)
-        _ = self.timer_display.config(text=f"Time: {self.timeout}")
+        if self.display:
+            _ = self.error_message.config(text="")
+            self._redraw_tkinter_grid()
+            self.after_timer_decrement = self.display.after(1000, self._decrement_timer)
+            _ = self.timer_display.config(text=f"Time: {self.timeout}")
+
+        if self.player_turn == 2 and not self.headless:
+            bot = HalmaBot2000(
+                master_game=self, thinking_time=self.timeout, event_loop=self.display
+            )
+            robo_move = bot.determine_best_move()
+            self._process_move_input(robo_move=robo_move)
 
     def _is_valid_move(
         self,
@@ -378,18 +413,21 @@ class Halma:
             winner = 2 if self.player_turn == 1 else 1
             self._end_game(winner, timeout=True)
 
-        _ = self.timer_display.config(text=f"Time: {self.time_remaining}")
-        self.after_timer_decrement = self.display.after(1000, self._decrement_timer)
+        if self.display:
+            _ = self.timer_display.config(text=f"Time: {self.time_remaining}")
+            self.after_timer_decrement = self.display.after(1000, self._decrement_timer)
 
     def _set_player_scores(self):
         self.player_1_score = self._calculate_score(1)
         self.player_2_score = self._calculate_score(2)
-        _ = self.player_1_score_display.config(
-            text=f"P1 Score: {self.player_1_score:.2f}"
-        )
-        _ = self.player_2_score_display.config(
-            text=f"P2 Score: {self.player_2_score:.2f}"
-        )
+
+        if self.display:
+            _ = self.player_1_score_display.config(
+                text=f"P1 Score: {self.player_1_score:.2f}"
+            )
+            _ = self.player_2_score_display.config(
+                text=f"P2 Score: {self.player_2_score:.2f}"
+            )
 
     def _calculate_score(self, player: int):
         return sum(
@@ -418,13 +456,19 @@ class Halma:
             for camp_row_index, camp_col_index in opponent_camp_coordinates
         )
 
-        return 1 / closest_euclidean_distance if closest_euclidean_distance > 0 else 1
+        if closest_euclidean_distance == 0:
+            return 1.0
+
+        return 1 / (closest_euclidean_distance + 1)
 
     def _get_euclidean_distance(self, x1: int, y1: int, x2: int, y2: int) -> float:
         return (((x1 - x2) ** 2) + ((y1 - y2) ** 2)) ** (1 / 2)
 
     def _end_game(self, winning_player: int, timeout: bool = False):
         # remake display with endscreen info
+        if self.headless:
+            return None
+
         for widget in self.display.winfo_children():
             widget.destroy()
 
@@ -455,6 +499,92 @@ class Halma:
         )
 
         exit_button.grid(row=len(self.grid), column=len(self.grid), columnspan=2)
+
+
+class HalmaBot2000:
+    def __init__(self, master_game: Halma, thinking_time: int, event_loop: tk.Tk):
+        self.original_board: list[list[int]] = deepcopy(master_game.grid)
+        self.game: Halma = Halma(
+            len(self.original_board),
+            thinking_time * 999999,  # just a big number so unexpected swaps dont happen
+            "red",
+            self.original_board,
+            True,
+        )
+        self.timeout_set: bool = False
+        self.game._swap_turns()
+
+    def determine_best_move(self) -> str:
+        """Returns the best move found through searching"""
+        return self._determine_blind_best_move()
+
+    def _minimax_search(self, board: list[list[int]], max_depth: int):
+        """Searches for best move using minimax algorithm"""
+
+    def _determine_blind_best_move(self) -> str:
+        """Finds move with highest positive score delta for moves currently on the board"""
+        print("---------------------------------------")
+        possible_moves = self._get_all_possible_moves()
+
+        best_move: str = ""
+        best_move_score: float = -math.inf
+
+        for move in possible_moves:
+            initial_board = deepcopy(self.game.grid)
+            self.game._process_move_input(robo_move=move)
+            move_score = self._determine_board_quality()
+
+            print(move, move_score)
+
+            if move_score > best_move_score:
+                best_move = move
+                best_move_score = move_score
+
+            self._set_game_status(initial_board, players_turn=2)
+
+        print("---------------------------------------")
+
+        return best_move
+
+    def _get_all_possible_moves(self) -> list[str]:
+        result: list[str] = []
+        bot_pawn_coordinates = self._get_all_pawn_coordinates()
+        for row_index, col_index in bot_pawn_coordinates:
+            start = self._translate_indexes_to_coordinates(row_index, col_index)
+            self.game._select_piece(row_index, col_index)
+            end_coordinates = [
+                self._translate_indexes_to_coordinates(row_index, col_index)
+                for row_index, row in enumerate(self.game.grid_display)
+                for col_index, col in enumerate(row)
+                if col == 3
+            ]
+            result.extend([f"{start}->{end}" for end in end_coordinates])
+
+        return result
+
+    def _get_all_pawn_coordinates(
+        self,
+    ) -> list[tuple[int, int]]:
+        return [
+            (row_index, col_index)
+            for row_index, row in enumerate(self.game.grid)
+            for col_index, col in enumerate(row)
+            if col == 2
+        ]
+
+    def _determine_board_quality(self):
+        return self.game.player_2_score - self.game.player_1_score
+
+    def _translate_indexes_to_coordinates(self, row_index: int, col_index: int):
+        return f"{chr(ord('a') + col_index)}{row_index + 1}"
+
+    def _set_timeout(self):
+        self.timeout_set = True
+
+    def _set_game_status(self, new_board: list[list[int]], players_turn: int):
+        self.game.grid = new_board
+        if self.game.player_turn != players_turn:
+            self.game._swap_turns()
 
 
 if __name__ == "__main__":
